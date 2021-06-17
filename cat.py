d12341942bc0f3a5c3144c8eda6ba7070949bef0
soloma70 <83924341+soloma70@users.noreply.github.com>
2021-06-17 22:56:26 +0300

    Add files

---
+++ cat.py
@@ -0,0 +1,6 @@
+class Cat:
+    def __init__(self, name, gender, age):
+        self.name = name
+        self.gender = gender
+        self.age = age
+        
---
+++ testCats.py
@@ -0,0 +1,13 @@
+from cat import Cat
+
+cat1 = Cat("Барон", "Мальчик", 2)
+
+print("Имя:", cat1.name)
+print("Пол:", cat1.gender)
+print("Возраст:", cat1.age)
+
+cat2 = Cat("Сэм", "Мальчик", 2)
+print("--------")
+print("Имя:", cat2.name)
+print("Пол:", cat2.gender)
+print("Возраст:", cat2.age)
